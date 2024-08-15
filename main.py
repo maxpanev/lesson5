3bd7ce633cfa61e0c1ccc90aece28e6f5c0439bb
maxpanev <maxpanev170100@gmail.com>
2024-08-15 16:27:22 +0300

    add new code

--- main.py
+++ main.py
@@ -4,37 +4,31 @@ class Store:
         self.address = address    # Адрес магазина
         self.items = {}           # Пустой словарь для товаров и их цен
 
-    def add_item(self, item_name, price):
-        """Добавляет товар и его цену в словарь items."""
+    def add_item(self, item_name, price): #Добавляет товар и его цену в словарь items
         self.items[item_name] = price
 
-    def remove_item(self, item_name):
-        """Удаляет товар из словаря items."""
+    def remove_item(self, item_name): #Удаляет товар из словаря items
         if item_name in self.items:
             del self.items[item_name]
             print(f"Товар '{item_name}' удален из магазина.")
         else:
             print(f"Товар '{item_name}' не найден в магазине.")
 
-    def get_item_price(self, item_name):
-        """Возвращает цену товара, если он есть в словаре, иначе None."""
+    def get_item_price(self, item_name): #Возвращает цену товара, если он есть в словаре, иначе None
         return self.items.get(item_name)
 
-    def display_items(self):
-        """Выводит список всех товаров и их цен."""
+    def display_items(self): #Выводит список всех товаров и их цен
         for item_name, price in self.items.items():
             print(f"{item_name}: {price} руб за кг")
 
-    def update_price(self, item_name, new_price):
-        """Обновляет цену товара, если он есть в словаре, иначе выводит предупреждение."""
+    def update_price(self, item_name, new_price): #Обновляет цену товара, если он есть в словаре, иначе выводит предупреждение
         if item_name in self.items:
             self.items[item_name] = new_price
             print(f"\nЦена на {item_name} обновлена до {new_price} руб за кг.")
         else:
             print(f"Товар '{item_name}' не найден в магазине.")
 
-    def delete_item(self, item_name):
-        """Удаляет товар по названию и выводит предупреждение, если его нет."""
+    def delete_item(self, item_name): #Удаляет товар по названию и выводит предупреждение, если его нет
         if item_name in self.items:
             del self.items[item_name]
             print(f"\nТовар '{item_name}' успешно удален.")
@@ -42,7 +36,6 @@ class Store:
             print(f"Товар '{item_name}' не найден в магазине.")
 
 # Пример использования
-
 my_store = Store("Овощи и Фрукты", "ул. Пушкина 123")
 my_store2 = Store("Мебельный", "ул. Лермонтова 456")
 my_store3 = Store("Товары для дома", "ул. Есенина 78")
